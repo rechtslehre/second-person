c917213227edec20c1052d8c99ecd032ae7986cd
michaelt <what_is_it_to_do_anything@yahoo.com>
2015-09-01 17:13:50 -0400

    papers from second person seminar 2010

---
+++ a.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/bash
+
+
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/1.1_McMyler_Benjamin_2011_Testimony_Trust_and_Authority.pdf > 1.1_McMyler_Benjamin_2011_Testimony_Trust_and_Authority.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/1.2E_Arnauld_Antoine_und_Nicole_Pierre_Logic_or_the_Art_of_Thinking_Fourth_Part_Chapter_12.pdf > 1.2E_Arnauld_Antoine_und_Nicole_Pierre_Logic_or_the_Art_of_Thinking_Fourth_Part_Chapter_12.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/1.2G_Arnauld_Antoine_und_Nicole_Pierre_Die_Logik_oder_die_Kunst_des_Denkens_Teil_IV_Kap._12-14.pdf > 1.2G_Arnauld_Antoine_und_Nicole_Pierre_Die_Logik_oder_die_Kunst_des_Denkens_Teil_IV_Kap._12-14.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/1.2F_Arnauld_Antoine_und_Nicole_Pierre_La_logique_ou_l_art_de_penser_part_IV_ch._12-14.pdf > 1.2F_Arnauld_Antoine_und_Nicole_Pierre_La_logique_ou_l_art_de_penser_part_IV_ch._12-14.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/1.2L_Arnauld_Antoine_und_Nicole_Pierre_Logica_sive_Ars_Cogitandi._IV_Cap_12-14.pdf > 1.2L_Arnauld_Antoine_und_Nicole_Pierre_Logica_sive_Ars_Cogitandi._IV_Cap_12-14.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/1.3_Hume_David_Enquiries_Section_10_Of_Miracles.pdf > 1.3_Hume_David_Enquiries_Section_10_Of_Miracles.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/1.4_McDowell_John_Knowledge_by_Hearsay.pdf > 1.4_McDowell_John_Knowledge_by_Hearsay.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/1.5_Kant_Immanuel_Was_heisst_Aufklaerung.pdf > 1.5_Kant_Immanuel_Was_heisst_Aufklaerung.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/1.6_Gelfert_Kant_on_Testimony.pdf > 1.6_Gelfert_Kant_on_Testimony.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/2.8_Moran_Richard_2011_Introduction_to_book.pdf > 2.8_Moran_Richard_2011_Introduction_to_book.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/2.7_Moran_Richard_2011_Testimony_and_the_2nd_Person.pdf > 2.7_Moran_Richard_2011_Testimony_and_the_2nd_Person.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/2_Moran_Richard_2011_Sincerity_Expanded.pdf > 2_Moran_Richard_2011_Sincerity_Expanded.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/Anscombe_G._E._M._What_Is_It_to_Believe_Someone.pdf > Anscombe_G._E._M._What_Is_It_to_Believe_Someone.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/2.2_Anscombe_Rules_Rights_and_Promises.pdf > 2.2_Anscombe_Rules_Rights_and_Promises.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/2.3_Hornsby_Jennifer_1994_Illocution_and_Its_Significance.pdf > 2.3_Hornsby_Jennifer_1994_Illocution_and_Its_Significance.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/2.5_Moran_Richard_2005_Problems_of_Sincerity.pdf > 2.5_Moran_Richard_2005_Problems_of_Sincerity.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/Cavell_Claim_of_Reaso.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/2.4_Moran_Richard_2005_Getting_Told_and_Being_Believed.pdf > 2.4_Moran_Richard_2005_Getting_Told_and_Being_Believed.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/Cavell_on_Outsiders_and_Other.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/3.3_Weinrib_Ernest_1995_Private_Law_ch3.pdf > 3.3_Weinrib_Ernest_1995_Private_Law_ch3.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/3.4_Long_Island_Railroad_Court_Case.pdf > 3.4_Long_Island_Railroad_Court_Case.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/3.1_Stone_Martin_2001_The_Significance_of_Doing_and_Suffering.pdf > 3.1_Stone_Martin_2001_The_Significance_of_Doing_and_Suffering.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/3.2_Stone_Positivism_as_Moral_Idea_Tufts_Talk.pdf > 3.2_Stone_Positivism_as_Moral_Idea_Tufts_Talk.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/3.7E_Kant_Immanuel_Metaphysics_of_Morals_selection.pdf > 3.7E_Kant_Immanuel_Metaphysics_of_Morals_selection.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/3.7G_Kant_Immanuel_Metaphysische_Anfangsgruende_der_Rechtslehre.pdf > 3.7G_Kant_Immanuel_Metaphysische_Anfangsgruende_der_Rechtslehre.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/Ripstein_Arthur_2009_Force_and_Freedom_Chs_1_2.pdf > Ripstein_Arthur_2009_Force_and_Freedom_Chs_1_2.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/3.51_Weinrib_Ernest_1995_Private_Law_ch1.pdf > 3.51_Weinrib_Ernest_1995_Private_Law_ch1.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/3.52_Weinrib_Ernest_1995_Private_Law_ch2.pdf > 3.52_Weinrib_Ernest_1995_Private_Law_ch2.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/3.6_Ethica_Nicomachea_V.pdf > 3.6_Ethica_Nicomachea_V.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/Die_Nicomachische_Ethik_Buch_V.pdf > Die_Nicomachische_Ethik_Buch_V.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/Die_Nikomachische_Ethik_Buch_V_Meiner_.pdf > Die_Nikomachische_Ethik_Buch_V_Meiner_.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/Nicomachean_Ethics_Book_V_Barnes_.pdf > Nicomachean_Ethics_Book_V_Barnes_.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/3.9_FeinbergTheNatureandValueofRights.pdf > 3.9_FeinbergTheNatureandValueofRights.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/4.1_Wanderer_Jeremy_2011_Addressing_Testimonial_Injustice.pdf > 4.1_Wanderer_Jeremy_2011_Addressing_Testimonial_Injustice.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/4.2_Alethic_Holdings_J.Wanderer.pdf > 4.2_Alethic_Holdings_J.Wanderer.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/4.3_Kukla_Lance_2009_Yo_and_Lo_chapter_7.pdf > 4.3_Kukla_Lance_2009_Yo_and_Lo_chapter_7.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/5.1_Thompson_Michael_What_is_it_to_Wrong_Someone_A_Puzzle_about_Justice.pdf > 5.1_Thompson_Michael_What_is_it_to_Wrong_Someone_A_Puzzle_about_Justice.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/Darwall_Stephen_2006_The_Second_Person_Standpoint_chs._1_2.pdf > Darwall_Stephen_2006_The_Second_Person_Standpoint_chs._1_2.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/Roedl_Sebastian_2010_You_and_I.pdf > Roedl_Sebastian_2010_You_and_I.pdf
+curl http://www.nationalhumanitiescenter.org/edforums/file.php/51/READINGS/5.5_Darwall_Authority_and_Second-Person_Reasons_for_Acting.pdf > 5.5_Darwall_Authority_and_Second-Person_Reasons_for_Acting.pdf
+
